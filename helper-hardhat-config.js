cb242258e2eb0d98aae383eea6077d597fa22209
bsrammca <bsrammca@gmail.com>
2022-08-25 09:29:20 -0700

    helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -4,7 +4,7 @@ const networkConfig = {
         vrfCoordinatorV2: "0x271682DEB8C4E0901D1a1550aD2e64D568E69909",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-        subscriptionId: "10080", // need this
+        subscriptionId: "10390", // need this
         //need to setup keepers for our contract!
         callbackGasLimit: "500000", //500,000
         interval: "30",
